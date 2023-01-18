14b229fba15e06d1efb1271047b65486ebc65e17
Mariana  Serrão <marianaserrao@Marianas-MacBook-Air.local>
2023-01-18 22:04:40 +0100

    added the fucntion to prompt password input and conditional statements for the selecting a number between 10 and 64. didn't work so need to review this.

--- starter/script.js
+++ starter/script.js
@@ -9,25 +9,65 @@ var numericCharacters = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9'];
 var lowerCasedCharacters = ['a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z'
 ];
 
-
 // Array of uppercase characters to be included in password
 var upperCasedCharacters = [
   'A','B','C','D','E','F','G','H','I','J','K','L','M','N','O','P','Q','R','S','T','U','V','W','X','Y','Z'
 ];
 
+
+//declare variables for each array 
+
+var numberCharacters;
+var special;
+var numbers;
+var lower;
+var upper;
+
+
+
+
 //Add an event listener to the button 
+// this returns the first element within the document that matches the specified selector. 
+var generateButton = document.querySelector("#generate");
+
+//this is showing an error say that "cannot read properties of null(reading'addEventListner")
+generateButton.addEventListener("click", function () {
+  getPasswordOptions()
+  console.log("it's working!")
+})
 
 
 //Function to prompt for password input
-function getPasswordLength () {
+function getPasswordOptions () {
   // to clear array with each click of button
-  generatorArr.length = 0;
-  var passwordLength = parseInt (prompt ("Please select how many characters you want your password to have", "Between 10 - 64"));
-  if (passwordLength >= 10 && passwordLength <= 64){
-  console.log(passwordLength);
+  numberCharacters = prompt ("Please select how many characters you want your password to have");
+  console.log(numberCharacters)
+//using isNaN() function because it determines wheter a value is not a number. 
+if (isNaN(numberCharacters)){
+  alert("please pick a number between 10 and 64")
   getPasswordOptions()
+}
+//else if for conditional statements for each alert, tried to join both alerts into one
+//creating two else if statements because not working combined
+else if (numberCharacters <10) {
+  alert 
+  
+  else if (numberCharacters >65){
+alert("Password length must be at least 10 characters and no more than 64")
+getPasswordOptions()
+}
+// using confirm to display the message box with an ok and cancel button, automatically returns true if the user clicks ok otherwise returns false
+else { 
+  characterOptions ()}
+  function characterOptions() {
+    lower = confirm ("Do you want to include lowercase characters?")
+    upper = confirm ("Do you want to include uppercase characters?")
+    numbers = confirm ("Do you want to include numbers?")
+    special = confirm ("Do you want to include special characters?")
   }
 }
+
+
 
 // // Function to prompt user for password options
 // function getPasswordOptions(prompt) {
